628763a5a7e6848907fa0cf1dc786851e5162cb3
Alex Sleighter <alex@orderup.com>
2015-02-09 02:34:57 -0500

    try sending as object

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/', function(request, response) {
 app.post('/on', function(req, resp) {
   wss.broadcast = function broadcast(data) {
     wss.clients.forEach(function each(client) {
-      client.send({'on'});
+      client.send({message:'on'});
     });
   };
   wss.broadcast = function broadcast(data) {
